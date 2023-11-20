5bef36edba60dda9544114482bfd1f9ed6c97dc2
Sebastian Salmeron <sebtics@hotmail.com>
2023-11-20 15:26:12 -0600

    Archivos Index

---
+++ Back-End/TemplatesV2/index/script.js
@@ -0,0 +1,4 @@
+document.getElementById('menu-btn').addEventListener('click', function() {
+    var menu = document.getElementById('menu');
+    menu.style.display = menu.style.display === 'none' ? 'block' : 'none';
+});
