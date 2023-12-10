459cbce13a83671f3d84a4f8ff91502eb9e15171
LeonardoCiencias <42622068+LeonardoCiencias@users.noreply.github.com>
2023-12-10 00:41:02 -0600

    Delete Back-End directory

--- Back-End/auth.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, blueprint , render_template
-from flask_sqlalchemy import SQLAlchemy
-from . import models
-
-bp = blueprint('auth', __name__, url_prefix='/auth')
-
-@bp.route('/')
-def login():
-    return render_template('index.html')
--- Back-End/templates/index/script.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('menu-btn').addEventListener('click', function() {
-    var menu = document.getElementById('menu');
-    menu.style.display = menu.style.display === 'none' ? 'block' : 'none';
-});
